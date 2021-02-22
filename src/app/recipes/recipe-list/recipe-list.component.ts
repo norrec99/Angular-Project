63fb7564551657e8a356b2a42c6c875a247cad73
Cagatay Soyer <cagataysoyer@gmail.com>
2021-02-22 15:44:57 +0300

    Managing recipes in a recipe service

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,5 +1,6 @@
 import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 import { Recipe } from '../recipe.model';
+import { RecipeService } from '../recipe.service';
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
@@ -7,21 +8,13 @@ import { Recipe } from '../recipe.model';
 })
 export class RecipeListComponent implements OnInit {
   @Output() recipeWasSelected = new EventEmitter<Recipe>();
-  recipes: Recipe[] = [
-    new Recipe(
-      'A Test Recipe',
-      'This is a test',
-      'https://joyfoodsunshine.com/wp-content/uploads/2016/09/easy-pizza-casserole-recipe-4-500x500.jpg'
-    ),
-    new Recipe(
-      'Another Test Recipe',
-      'This is a test',
-      'https://joyfoodsunshine.com/wp-content/uploads/2016/09/easy-pizza-casserole-recipe-4-500x500.jpg'
-    ),
-  ];
-  constructor() {}
+  recipes: Recipe[];
 
-  ngOnInit(): void {}
+  constructor(private recipeService: RecipeService) {}
+
+  ngOnInit() {
+    this.recipes = this.recipeService.getRecipes();
+  }
 
   onRecipeSelected(recipe: Recipe) {
     this.recipeWasSelected.emit(recipe);
--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,1 +1,20 @@
-export class RecipeService {}
+import { Recipe } from './recipe.model';
+
+export class RecipeService {
+  private recipes: Recipe[] = [
+    new Recipe(
+      'A Test Recipe',
+      'This is a test',
+      'https://joyfoodsunshine.com/wp-content/uploads/2016/09/easy-pizza-casserole-recipe-4-500x500.jpg'
+    ),
+    new Recipe(
+      'Another Test Recipe',
+      'This is a test',
+      'https://joyfoodsunshine.com/wp-content/uploads/2016/09/easy-pizza-casserole-recipe-4-500x500.jpg'
+    ),
+  ];
+
+  getRecipes() {
+    return this.recipes.slice();
+  }
+}
